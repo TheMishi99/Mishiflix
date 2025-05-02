a943393ae025d150d2309d1fadcfc32a3b424110
Matias Demian Sayago <matiasgd99@gmail.com>
2025-05-02 06:28:50 -0300

    Fix: Profile Page

--- src/app/auth/profile/page.tsx
+++ src/app/auth/profile/page.tsx
@@ -30,69 +30,14 @@ export default function ProfilePage() {
       className="flex flex-col sm:flex-row justify-center items-center p-2 gap-2 bg-zinc-900 rounded-xl"
     >
       {userLogged && (
-        <>
-          <div className="flex flex-col justify-center items-center p-2 gap-2">
-            <Avatar
-              src={userLogged.avatar}
-              alt={userLogged.username}
-              className="size-32"
-            />
-            <p>{userLogged.username}</p>
-          </div>
-
-          <div className="flex flex-col justify-center items-start p-2 gap-2">
-            <p>
-              {
-                titlesByLanguage[language as keyof typeof titlesByLanguage]
-                  .favoriteMovies
-              }
-              :{" "}
-              {userLogged.favoriteMovies.reduce<ReactNode[]>(
-                (acc, favMov, index) => {
-                  acc.push(
-                    <Link
-                      key={favMov.id + favMov.title}
-                      href={`/movies/${favMov.id}`}
-                      className="text-red-600"
-                    >
-                      {favMov.title}
-                    </Link>
-                  );
-                  if (index < userLogged.favoriteMovies.length - 1) {
-                    acc.push(", ");
-                  }
-                  return acc;
-                },
-                []
-              )}
-            </p>
-            <p>
-              {
-                titlesByLanguage[language as keyof typeof titlesByLanguage]
-                  .favoriteSeries
-              }
-              :{" "}
-              {userLogged.favoriteSeries.reduce<ReactNode[]>(
-                (acc, favSer, index) => {
-                  acc.push(
-                    <Link
-                      key={favSer.id + favSer.title}
-                      href={`/series/${favSer.id}`}
-                      className="text-red-600"
-                    >
-                      {favSer.title}
-                    </Link>
-                  );
-                  if (index < userLogged.favoriteSeries.length - 1) {
-                    acc.push(", ");
-                  }
-                  return acc;
-                },
-                []
-              )}
-            </p>
-          </div>
-        </>
+        <div className="flex flex-col justify-center items-center p-2 gap-2">
+          <Avatar
+            src={userLogged.avatar}
+            alt={userLogged.username}
+            className="size-32"
+          />
+          <p>{userLogged.username}</p>
+        </div>
       )}
     </div>
   );
